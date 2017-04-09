55011fbb0a2c625f06c332bc27d5e3820f892583
Renata Argerique <renatapva@gmail.com>
2017-04-09 18:33:18 -0300

    Add control to have more threads, and comment mutex to run tests

--- main.c
+++ main.c
@@ -1,8 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#define N 10000
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int saldo = 100;
+//pthread_mutex_t m;
 
-int main(int argc, char *argv[]) {
-	return 0;
+void *retira(void *str) {
+	int i, b;
+ 	for (i=0; i<N; i++) {
+ 		//pthread_mutex_lock(&m);
+ 		b = saldo; 
+		b = b - 1; 
+		saldo = b;
+ 		//pthread_mutex_unlock(&m);
+ 	}
+ 	printf("\nExecutou Retira. Saldo = %d", saldo);
+}
+
+void *deposita(void *str) {
+ 	int i, a;
+ 	for(i=0; i<N; i++) {
+ 		//pthread_mutex_lock(&m);
+ 		a = saldo; 
+		a = a + 1; 
+		saldo = a;
+ 		//pthread_mutex_unlock(&m);
+ 	}
+ 	printf("\nExecutou Deposita. Saldo = %d", saldo);
+}
+
+int main() {
+	int tDeposita = 1;
+ 	int tRetira = 1;
+	
+	pthread_t thread[tDeposita+tRetira];
+ 	//pthread_mutex_init(&m, NULL);
+ 	
+ 	int i;
+ 	
+ 	for(i=0; i<tDeposita; i++){
+ 		pthread_create(&thread[i], NULL, deposita, NULL);	
+	}
+	
+	for(i=tDeposita; i<tDeposita+tRetira; i++){
+		pthread_create(&thread[i], NULL, retira, NULL);
+	}
+ 	
+ 	for(i=0; i<tDeposita+tRetira; i++){
+ 		pthread_join(thread[i], NULL);	
+	}
+	/*if ((pthread_create(&thid1, NULL, deposita, NULL) != 0) || (pthread_create (&thid2, NULL, retira, NULL) != 0)) {
+ 		printf("Error!\n");
+ 		exit(0);
+ 	}
+ 	
+	pthread_join(thid1, NULL);
+ 	pthread_join(thid2, NULL); */
+ 	
+ 	printf("\nSALDO ATUAL = %d\n", saldo);
+ 	return 0;
 }
